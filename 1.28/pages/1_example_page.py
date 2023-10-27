99cecdcd21566fbbc93522752f7fb7c91d170b43
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-10-27 16:56:18 -0400

    Update 1_example_page.py

--- 1.28/pages/1_example_page.py
+++ 1.28/pages/1_example_page.py
@@ -4,9 +4,9 @@ from PIL import Image
 
 st.write(os.getcwd())
 
-path = os.path.dirname(__file__)
-my_file = path+'/image.jpg'
-image = Image.open(my_file)
+# path = os.path.dirname(__file__)
+# my_file = path+'/image.jpg'
+image = Image.open('/mount/src/debug-demo/1.28/pages/image.jpg')
 st.write(my_file)
 
 st.image(image)
