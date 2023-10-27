e1d8172036dc90862d32e59f77ba260724e78daa
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-10-27 16:56:50 -0400

    Update 1_example_page.py

--- 1.28/pages/1_example_page.py
+++ 1.28/pages/1_example_page.py
@@ -7,6 +7,6 @@ st.write(os.getcwd())
 # path = os.path.dirname(__file__)
 # my_file = path+'/image.jpg'
 image = Image.open('/mount/src/debug-demo/1.28/pages/image.jpg')
-st.write(my_file)
+# st.write(my_file)
 
 st.image(image)
