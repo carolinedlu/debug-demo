4045c42ae886aa042df3beac087ab1d72daad9dc
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-11-07 10:05:25 -0500

    Update 1_example_page.py

--- 1.28/pages/1_example_page.py
+++ 1.28/pages/1_example_page.py
@@ -4,9 +4,10 @@ from PIL import Image
 
 st.write(os.getcwd())
 
-# path = os.path.dirname(__file__)
-# my_file = path+'/image.jpg'
-image = Image.open('/mount/src/debug-demo/1.28/pages/image.jpg')
-# st.write(my_file)
+path = os.path.dirname(__file__)
+my_file_path = path+'/image.jpg'
+st.write(my_file_path)
+# image = Image.open('/mount/src/debug-demo/1.28/pages/image.jpg')
+# # st.write(my_file)
 
-st.image(image)
+# st.image(image)
